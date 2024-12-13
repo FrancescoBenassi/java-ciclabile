112e507841d126ce4bc377bd4dadf90971338ea9
FrancescoBenassi <francybenna77@gmail.com>
2024-12-13 18:49:06 +0100

    feat: added builder and methods

--- src/List.java
+++ src/List.java
@@ -1,31 +1,72 @@
 public class List {
 
-    protected int[] numberList = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    int indexArray = 0;
+    private int[] numberList;
+    private int indexArray = 0;
 
-    // public int getTuttiElementi(){
-    // for (int number : numberList) {
-    // return numberList[number];
-    // }
+    // Costruttore che passa come parametro un array di interi
 
-    // }
+    public List(int[] numberList) {
+        this.numberList = numberList;
+    }
+
+    // Costruttore che non passa alcun dato
+
+    public List() {
+
+    }
+
+    // Metodo che ritorna la lunghezza dell'Array
+
+    public int getNumberListLenght() {
+        return numberList.length;
+    }
+
+    // Metodo per avere il valore di indexArray
+
+    public int getIndexArray() {
+        return indexArray;
+    }
+
+    // Metodo per settare a 0 il valore di indexArray
+
+    public void setResetIndexArray() {
+        indexArray = 0;
+    }
+
+    // Metodo per avere il valore successivo all'interno dell'Array
 
     public int getElementoSuccessivo() {
-        indexArray++;
-        if (numberList.length >= indexArray) {
-            return numberList[-1 + indexArray];
+        this.indexArray++;
+        if (this.numberList.length >= indexArray) {
+            return this.numberList[-1 + indexArray];
         } else {
-            indexArray--;
-            return numberList[-1 + indexArray];
+            this.indexArray--;
+            return this.numberList[-1 + indexArray];
         }
     }
 
+    // Metodo per sapere se è presente l'elemento successivo all'interno dell'Array
+
     public boolean hasAncoraElementi() {
         boolean result = false;
-        if (numberList.length >= indexArray + 1) {
+        if (this.numberList.length >= this.indexArray + 1) {
             result = true;
         }
         return result;
+    }
+
+    // Metodo per aggiungere un valore all'interno dell'Array
+
+    public void setAddElemento(int number) {
+        int[] newArray = new int[this.numberList.length + 1];
+        int numberListLenght = this.numberList.length;
+        for (int i = 0; i < numberListLenght; i++) {
+            newArray[i] = this.numberList[i];
+        }
+        newArray[numberListLenght] = number;
+
+        this.numberList = newArray;
+
     }
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,40 @@
 public class Test {
 
     public static void main(String[] args) {
-        
-       List listNumber = new List();
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.getElementoSuccessivo());
-       System.out.println(listNumber.hasAncoraElementi());
+        int[] numberList = { 10, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List numberListArrayList = new List(numberList);
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        numberListArrayList.setAddElemento(30);
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+        numberListArrayList.setAddElemento(17);
+        System.out.println(numberListArrayList.getElementoSuccessivo());
+        System.out.println(numberListArrayList.hasAncoraElementi());
+
+        // Ciclo per leggere tutti i valori all'interno dell'Array numberListArrayList
+
+        if (numberListArrayList.getIndexArray() != 0) {
+            numberListArrayList.setResetIndexArray();
+            for (int i = 0; i < numberListArrayList.getNumberListLenght(); i++) {
+                System.out.println(numberListArrayList.getElementoSuccessivo());
+            }
+        }
     }
 }
